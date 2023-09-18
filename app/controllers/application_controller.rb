0a5f8e4634c53fea450e03447d8da77a5165ca83
Enning94 <enningfestus@gmail.com>
2023-09-18 18:13:57 +0000

    setup devise for authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
-end
+    before_action :authenticate_user!
+    protect_from_forgery with: :null_session
+  
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    respond_to :html, :json
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+        user_params.permit(:name, :email, :password, :password_confirmation)
+      end
+    end
+  end
