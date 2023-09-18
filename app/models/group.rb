747fedeca0a2efa6f48031c8e998726c4c1e5db6
Enning94 <enningfestus@gmail.com>
2023-09-18 12:12:34 +0000

    created project models and migrations

---
+++ app/models/group.rb
@@ -0,0 +1,9 @@
+class Group < ApplicationRecord
+  belongs_to :user
+  has_many :group_entities
+  has_many :entities, through: :group_entities
+
+  
+  validates :name, presence: true
+  validates :icon, presence: true
+end
---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    has_many :groups
+    has_many :entities
+    validates :name, presence: true
+end
