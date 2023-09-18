eca3c5f07d9a774d4a8732990a8a88e6095efa4e
Enning94 <enningfestus@gmail.com>
2023-09-18 18:27:18 +0000

    Fix linter errors

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,6 @@ class Group < ApplicationRecord
   has_many :group_entities
   has_many :entities, through: :group_entities
 
-  
   validates :name, presence: true
   validates :icon, presence: true
 end
