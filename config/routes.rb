fe98ba197b5c38206ec5a904101bd5ef957022f4
Enning94 <enningfestus@gmail.com>
2023-09-20 19:19:01 +0000

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  resources :splashes
   root "groups#index"
 
   devise_for :users
     resources :users do
-      resources :groups
-      resources :entities
- 
+      resources :groups do
+        resources :entities
+      end
     end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
