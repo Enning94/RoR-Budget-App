019b05f32f5acb0f17be8faf60844750abc0f9b5
Enning94 <enningfestus@gmail.com>
2023-09-20 19:14:33 +0000

    modify application_controller and layout/application files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,15 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+ 
   protect_from_forgery with: :null_session
-
   before_action :configure_permitted_parameters, if: :devise_controller?
   respond_to :html, :json
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
       user_params.permit(:name, :email, :password, :password_confirmation)
     end
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    splashes_path # Change this to the path you want to redirect to after logout
   end
 end
