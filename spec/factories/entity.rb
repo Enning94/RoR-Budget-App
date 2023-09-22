1072474fba3feba0923cf4e65772f359c446620f
Enning94 <enningfestus@gmail.com>
2023-09-22 18:25:11 +0000

    created factories for test

--- spec/factories/entity.rb
+++ spec/factories/entity.rb
@@ -1,9 +1,8 @@
 # spec/factories/entities.rb
 FactoryBot.define do
-    factory :entity do
-      association :author, factory: :user
-      name { 'Rice' }
-      amount { 5 }
-    end
+  factory :entity do
+    association :author, factory: :user
+    sequence(:name) { |n| "Entity #{n}" }
+    amount { 5 }
   end
-  
+end
